22f7bf1c0ac017bd4a2314b188356dd4c69595fb
Pomax <pomax@nihongoresources.com>
2014-05-22 11:12:33 -0700

    mmmmmassages

--- bin/server.js
+++ bin/server.js
@@ -3,9 +3,19 @@ if ( process.env.NEW_RELIC_ENABLED ) {
   require( "newrelic" );
 }
 
-var config = require("nconf")
-    .argv()
-    .file({file:__dirname+"/../config.json"});
+var nconf = require("nconf");
+var redirect = require('../lib/redirect');
 
-redirect = require('../lib/redirect')(config.get('redirects'), config.get('port'));
+// load the config from config.json
+var config = nconf.argv().file({file:__dirname+"/../config.json"});
+
+
+// extract the values we'll be passing to redirect()
+var redirectList = config.get('redirect');
+var proxyList = config.get('monkeys');
+var redirectPort = config.get('port');
+
+// check the redirects
+redirect(redirectList, redirectPort);
+
 console.log('redirect running on port %d', redirect.address().port);
